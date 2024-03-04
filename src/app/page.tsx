e76d1e837e9127acd751df3edd6a15f51a129abb
Kailash1233 <kailash61203@gmail.com>
2024-03-04 23:55:35 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
-// import { GridBackgroundDemo } from "./background-ui";
-import { BackgroundBeamsDemo } from "./bg-beam";
+import { GridBackgroundDemo } from "./background-ui";
+// import { WavyBackground } from "@/components/ui/wavy-background";
+// import { BackgroundBeamsDemo } from "./bg-beam";
 // import { BackgroundBoxesDemo } from "./bg-box";
 // import { GoogleGeminiEffectDemo } from "./new-ui";
+// import { FloatingNavDemo } from "./navbar";
+// import { SparklesPreview } from "./sparkles";
 
 export default function Home() {
   return (
     <main>
       {/* <GoogleGeminiEffectDemo />
-      <GridBackgroundDemo /> */}
-      {/* <BackgroundBoxesDemo /> */}
+      <SparklesPreview />
+      <FloatingNavDemo /> */}
+      <GridBackgroundDemo />
+      {/* <BackgroundBoxesDemo />
       <BackgroundBeamsDemo />
+      <WavyBackground /> */}
     </main>
   );
 }
